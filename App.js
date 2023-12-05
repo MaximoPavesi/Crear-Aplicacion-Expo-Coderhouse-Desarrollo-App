4ec9abadd62b8cc349a62697bf4a771f2afffd20
Maximo Pavesi <machipavesi@gmail.com>
2023-12-05 20:29:47 -0300

    lista optimizada

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Modal } from 'react-native'
+import { useState } from 'react'
+import CustomModal from './components/CustomModal'
+import CustomInput from './components/CustomInput'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola coder!</Text>
-      <StatusBar style="auto" />
+  const [textItem, setTextItem] = useState('')
+  const [itemList, setItemList] = useState([])
+  const [itemSelectedToDelete, setItemSelectedToDelete] = useState({})
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const onChangeTextHandler = (text) => {
+    setTextItem(text)
+    //console.log(text)
+  }
+
+  const addItemToList = () => {
+    setItemList(prevState => [...prevState, { id: Math.random().toString(), value: textItem }])
+    //console.log(itemList)
+    setTextItem('')
+  }
+
+  const onSelectItemHandler = (id) => {
+    setModalVisible(!modalVisible)
+    setItemSelectedToDelete(itemList.find((item) => item.id === id))
+  }
+
+  const onDeleteItemHandler = () => {
+    setItemList(itemList.filter((item) => item.id !== itemSelectedToDelete.id))
+    setModalVisible(!modalVisible)
+  }
+
+  const renderListItem = ({ item }) => (
+    <View style={styles.itemList}>
+      <Text>{item.value}</Text>
+      <Button title="x" onPress={() => onSelectItemHandler(item.id)} />
     </View>
+  )
+
+
+  return (
+    <>
+      <View style={styles.container}>
+        <CustomInput
+          placeholderProp="Ingresá la tarea"
+          textItemProp={textItem}
+          onChangeTextHandlerEvent={onChangeTextHandler}
+          addItemToListEvent={addItemToList}
+        />
+        <FlatList
+          data={itemList}
+          renderItem={renderListItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <CustomModal
+        animationTypeProp="slide"
+        isVisibleProp={modalVisible}
+        itemSelectedProp={itemSelectedToDelete}
+        onDeleteItemHandlerEvent={onDeleteItemHandler}
+        setModalVisibleEvent={setModalVisible}
+      />
+    </>
   );
 }
 
@@ -14,7 +68,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30
   },
+
+  itemList: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    margin: 10,
+    backgroundColor: "#FA3232",
+    borderRadius: 10,
+  },
+
 });
